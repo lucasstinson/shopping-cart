178f83a2645295726cf28b7ece2e756b79bf8dad
Luke <user@pop-os.localdomain>
2023-03-29 12:06:38 -0500

    add github link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import githubLogo from "../assets/images/github.png";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="footer-container">
-      <img className="footer-logo" src={githubLogo} alt="github logo"></img>
-      <div className="footer-text">Developed by lucasstinson</div>
+      <Link to={"https://github.com/lucasstinson/"} className="github-link">
+        <img className="footer-logo" src={githubLogo} alt="github logo"></img>
+        <div className="footer-text">Developed by lucasstinson</div>
+      </Link>
     </div>
   );
 };
